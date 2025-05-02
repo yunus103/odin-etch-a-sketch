7e69a655830192d0a9c6c82de98e9edcafb309ae
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2025-05-02 13:20:53 +0700

    Dynamic grid button by input and hover effect is implemented

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 const container = document.querySelector(".container");
+const btnSet = document.querySelector("#set");
 
-let grid = 16;
+let defaultGrid = 16;
 
-const boxSize = 600 / grid;
+createGrid(defaultGrid);
 
-for(let i = 0; i < grid*grid; i++){
-    const div = document.createElement("div");
-    div.style.width = boxSize + "px";
-    div.style.height = boxSize + "px";
-    div.classList.add("box");
-    container.append(div);
-}
+btnSet.addEventListener("click", () => {
+    const grid = Number(document.querySelector("#grid-count").value);
+    
+    if(!(grid < 1 || grid > 100))
+    {
+        container.replaceChildren();
+        createGrid(grid);
+    }
+});
+
+
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  function createGrid(number){
+    let boxSize = 600 / number;
+    for(let i = 0; i < number*number; i++){
+        const div = document.createElement("div");
+        div.style.width = boxSize + "px";
+        div.style.height = boxSize + "px";
+    
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = getRandomColor();
+        });
+        div.classList.add("box");
+        container.append(div);
+    }
+  }
